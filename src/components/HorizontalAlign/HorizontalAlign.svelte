60c3e31941291d208fcf069b79ebcef864c256b4
DenzeySenpai <marksolante221@gmail.com>
2024-10-10 17:56:28 +0800

    first commit

---
+++ src/components/HorizontalAlign/HorizontalAlign.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import "./style.css";
+  export let padding = "0px";
+  export let align = "center";
+  export let margin = "auto";
+</script>
+
+<div class="wrapper-wscvbjop-23ergh">
+  <slot></slot>
+</div>
